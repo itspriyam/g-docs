d23f141d0bdc75aee42c8849910c9c693c19e5fc
itspriyam <itspriyambajpai@gmail.com>
2023-05-22 23:14:37 +0530

    Google docs clone project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Editor from './components/Editor';
+import Header from './components/Header'
+import './App.css';
+
+function App() {
+  return (
+     <>
+     <Header/>
+      <Editor/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header=()=>{
+    return(
+        <ul className="heading-list">
+            <li>File</li>
+            <li>Edit</li>
+            <li>View</li>
+            <li>Insert</li>
+            <li>Format</li>
+            <li>Tools</li>
+        </ul>
+    )
+}
+export default Header;
